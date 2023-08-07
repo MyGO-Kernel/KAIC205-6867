9c4a639859944e984930cb5571c85bac55fa3caf
01soni247 <01soni247@gmail.com>
2023-08-07 17:53:16 +0700

    Big core : 850Mhz-2200Mhz Lite core : 400Mhz-1850Mhz GPU 11500Mhz

--- drivers/misc/mediatek/base/power/mt6768/mtk_gpufreq_core.h
+++ drivers/misc/mediatek/base/power/mt6768/mtk_gpufreq_core.h
@@ -17,13 +17,13 @@
 /**************************************************
  * MT6768/MT6767 : GPU DVFS OPP table Setting
  **************************************************/
-#define SEG_GPU_DVFS_FREQ0			(1000000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ1			(975000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ2			(950000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ3			(925000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ4			(900000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ5			(875000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ6			(850000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ0			(1150000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ1			(990000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ2			(965000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ3			(940000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ4			(915000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ5			(890000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ6			(865000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ7			(823000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ8			(796000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ9			(769000)	/* KHz */
